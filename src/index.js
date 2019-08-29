9ee00e22df927d837ac7abdb432846a4c9362f67
DaGuT <mangustalex11@gmail.com>
2019-08-29 23:13:53 +0100

    added components to index as well as made default style for body

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter, Route} from 'react-router-dom'; 
 
+//styling
+import './index.scss'
+
+//components/pages
 import WelcomePage from './app/components/WelcomePage';
 import Sidemenu from './app/components/Sidemenu';
+import Portfolio from './app/components/Portfolio';
+import AboutMe from './app/components/AboutMe';
 
 ReactDOM.render(
 <BrowserRouter> 
     <Route component={(props) => <Sidemenu {...props} />} />
-    <Route path="/" component={WelcomePage} />
+    <Route path="/" exact component={WelcomePage} />
+    <Route path="/portfolio" component={Portfolio} />
+    <Route path="/about-me" component={AboutMe} />
 </BrowserRouter>
 , document.getElementById('root'));
 
