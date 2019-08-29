bcb70c0f978146375bf63cff9cf1ef856bad6b92
DaGuT <mangustalex11@gmail.com>
2019-08-29 22:32:56 +0100

    reworked sidemenu

--- src/app/components/Sidemenu/Sidemenu.js
+++ src/app/components/Sidemenu/Sidemenu.js
@@ -4,8 +4,40 @@ import { Link } from 'react-router-dom'
 import "./Sidemenu.scss"
 
 export default class Sidemenu extends Component {
+
+    constructor() {
+        super();
+
+        this.state= {
+            underlineWidth:0,
+            underlineOffset:0
+        }
+
+        //binding events to this
+        this.moveUnderlineToActive = this.moveUnderlineToActive.bind(this);
+        this.handleMenuChange = this.handleMenuChange.bind(this);
+
+        //ref so that I can directly check properties of our underline href
+        this.sidemenuUnderlineHr = React.createRef();
+    }
+
+    moveUnderlineToActive() {
+        let el = document.querySelector('#Sidemenu .sidemenu-list .sidemenu-item.active');
+        this.setState({underlineWidth:el.offsetWidth, underlineOffset: el.offsetLeft});
+    }
+
+    handleMenuChange(e) {
+        this.setState({underlineWidth:e.target.offsetWidth, underlineOffset: e.target.offsetLeft});
+    }
+
+    //upon mounting component we need to place our href
+    componentDidMount() {
+        this.moveUnderlineToActive();
+    }
+
     render() {
 
+        //this links can be easily replaced with property links, but I dont need it
         let links = [
             {link:"/", name:'Home'},
             {link:"/portfolio", name: "Portfolio"},
@@ -14,11 +46,11 @@ export default class Sidemenu extends Component {
 
         return (
             <div id="Sidemenu">
-                <ul class="sidemenu-list">
+                <ul className="sidemenu-list">
                     {links.map((link,i) => 
-                        <li key={`menuitem-${i}`} className={`sidemenu-item item-${i+1} ${this.props.location.pathname===link.link ? 'active' : ''}`}><Link to={link.link}>{link.name}</Link></li>
+                        <li onMouseOver={this.handleMenuChange} onMouseLeave={this.moveUnderlineToActive} key={`menuitem-${i}`} className={`sidemenu-item ${this.props.location.pathname===link.link ? 'active' : ''}`}><Link to={link.link}>{link.name}</Link></li>
                     )}
-                    <hr />
+                    <hr ref={this.sidemenuUnderlineHr} style={{width:`${this.state.underlineWidth}px`, marginLeft:`${this.state.underlineOffset}px`}}/>
                 </ul>
             </div>
         )
