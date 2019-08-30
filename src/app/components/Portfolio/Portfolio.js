87f03f4116fb408e3f1ec3e67ddabfdda9058ae9
DaGuT <mangustalex11@gmail.com>
2019-08-30 17:07:25 +0100

    fixed memory leak with promise in portfolio component

--- src/app/components/Portfolio/Portfolio.js
+++ src/app/components/Portfolio/Portfolio.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import "./Portfolio.scss"
+import Deck from "../Deck"
+import makeCancellablePromise from "../../utils/makeCancellablePromise";
 
 const Portfolio = () => {
   // I decided to make stateless component, but then decided to add loading and
@@ -11,28 +13,40 @@ const Portfolio = () => {
   const [data,
     setData] = useState([]);
 
+  //componentDidMount and componentWillmount
   useEffect(() => {
-    const fetchData = async() => {
+    //this var is used by us so that we can cancel our promise
+    let cancellableResponse;
+
+    //function that is doing async call to fetch some url (in this case it will be portfolio)
+    //also it will report if there is error and if it is loading or not
+    const fetchData = async(url) => {
       //we're settings state to be loading
       setIsError(false);
       setIsLoading(true);
 
       //we fetch portfolio.json
       try {
-        let response = await fetch('/portfolio/portfolio.json');
-        //after fetching we need to get json answer
-        response = await response.json();
-        setData(response);
+        cancellableResponse = await makeCancellablePromise(fetch(url));
+        cancellableResponse = await makeCancellablePromise(cancellableResponse.json());
+        setData(cancellableResponse);
       } catch {setIsError(true);}
       setIsLoading(false);
     }
 
-    fetchData();
-    console.log(data);
-  },[]);
+    fetchData('/portfolio/portfolio.json');
+
+    //componentWillUnpmount
+    //we calcel our async call so that satte will not be changed in fechData
+    return (() => {
+      cancellableResponse && cancellableResponse.cancel && cancellableResponse.cancel();
+    });
+  }, []);
 
   return (
-    <div id="Portfolio">Hi!</div>
+    <div id="Portfolio">
+      <Deck data={data}/>
+    </div>
   );
 }
 
