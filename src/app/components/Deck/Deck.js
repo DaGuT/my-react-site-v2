30d6ee351b99a8cb4419e7100ec53a5a100de268
DaGuT <mangustalex11@gmail.com>
2019-08-30 17:05:33 +0100

    change Deck to use lighbox with non jquery required

--- src/app/components/Deck/Deck.js
+++ src/app/components/Deck/Deck.js
@@ -1,69 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Deckitem from './Deckitem.js';
 import './grid.css';
-import $ from 'jquery';
-import "magnific-popup";
-import "magnific-popup/dist/magnific-popup.css";
+import {BigPictureGallery} from "react-bigpicture";
 
-class Deck extends Component {
+const Deck = (props) => {
 
-  constructor(props) {
-    super(props);
+    let data = props.data;
 
-    this.state = {
-      data: this.props.data
-    };
-  }
-
-  //we first draw some empy blocks, then we parse data in this function and replace empty blocks with normal portfolio items
-  componentDidMount() {
-    this.initGallery();
-  }
-
-  //upon component update we update all portfolio items
-  componentDidUpdate() {
-    this.initGallery();
-  }
-
-  render() {
-    //may be I should hide functions like this?
     function drawDeck(data) {
       return data.map(item => <Deckitem key={item.src} item={item}/>);
     }
 
-    return (<div className="grid-container">{drawDeck(this.state.data)}</div>);
-  }
-
-  //misc functions
-  initGallery() {
-    $('.mf-popup').magnificPopup({
-      type: 'iframe',
-      iframe: {
-        patterns: {
-          pf: {
-            index: '',
-            id: function(url) {
-              return url;
-            },
-            src: '%id%'
-          }
-        }
-      },
-      gallery: {
-        enabled: true, // set to true to enable gallery
-
-        navigateByImgClick: true,
-
-        arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
-
-        tPrev: 'Previous (Left arrow key)', // title for left button
-        tNext: 'Next (Right arrow key)', // title for right button
-        tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
-      }
-
-    });
-  }
-
+    return (
+      <div>
+        <div className="grid-container">{drawDeck(data)}</div>
+      </div>
+    );
 }
 
 export default Deck;
--- src/app/components/Deck/Deckitem.js
+++ src/app/components/Deck/Deckitem.js
@@ -1,26 +1,30 @@
 import React, {Component} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import "./deck.css";
 import "./grid.css";
+import BigPicture from "react-bigpicture";
 
 class Deckitem extends Component {
   render() {
     return (
-      <div
-        href={this.props.item.src}
-        className={`
-        grr-6 grc-6
-        grr-lg-${this.props.item.rows || 1} grc-lg-${this.props.item.cols || 1} 
-        pf-item with-overlay mf-popup`}
+      <BigPicture
+        type="iframe"
+        caption={this.props.item.sitedesc
+        ? this.props.item.sitedesc
+        : ''}
+        src={this.props.item.src}
+        className={`active-image grr-6 grc-6 grr-lg-${this.props.item.rows || 1} grc-lg-${this.props.item.cols || 1} pf-item with-overlay`}
         style={{
         'backgroundImage': `url(${this.props.item.imgSrc})`
       }}>
+
         <div className="overlay">
           <div className="center-text">
             {this.props.item.sitename}
           </div>
         </div>
-      </div>
+
+      </BigPicture>
+
     );
 
   };
