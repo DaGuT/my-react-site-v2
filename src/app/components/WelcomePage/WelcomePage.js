8242bb1c1a3f097e3f2e47641e9fe2186d60e512
DaGuT <mangustalex11@gmail.com>
2019-08-30 17:06:31 +0100

    renamed import in welcome page

--- src/app/components/WelcomePage/WelcomePage.js
+++ src/app/components/WelcomePage/WelcomePage.js
@@ -1,13 +1,13 @@
 import React, { PureComponent } from 'react';
 
-import MainBlock from './Blocks/MainScreen';
+import MainScreen from './Blocks/MainScreen';
 import './WelcomePage.css';
 
 export default class WelcomePage extends PureComponent {
     render() {
         return (
             <div>
-                <MainBlock />
+                <MainScreen />
             </div>
         )
     }
